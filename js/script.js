ea1121649bfaf3968ff15140e51a6ef5d89f4bfb
Jeremy Niondo <jeremieniondo@gmail.com>
2023-07-06 13:29:45 +0000

    Updated

--- js/script.js
+++ js/script.js
@@ -2,20 +2,24 @@ var passwordEl = document.getElementById("password");
 var CpasswordEl = document.getElementById("confirmPassword");
 var form = document.getElementsByTagName("form")[0];
 
+const MIN_PASSWORD_LENGHT = 6;
 
-
-
-form.onsubmit = function validateForm(e) {
-    if (
-        passwordEl.length != 0
-    ) {
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (passwordEl.value.length >= MIN_PASSWORD_LENGHT) {
         if (passwordEl.value == CpasswordEl.value) {
-            alert("match")
+            location.reload();
         }
         else {
-            alert("Passwords do not match")
-            return
+            var confirmPassError = document.getElementById('pass-confirm-error');
+            confirmPassError.className = "passError";
+            confirmPassError.innerText = "Sorry, Password don't match !";
         }
     }
-
-}
+    else
+    {
+        var passwordError = document.getElementById('pass-error');
+        passwordError.className = "passError";
+        passwordError.innerText = `Too Short, ${MIN_PASSWORD_LENGHT} characters min !`;
+    }
+});
